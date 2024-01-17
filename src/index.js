909f8992a6ed9152fb6f1059f36159f9a356da60
Cristian Daniel Zuluaga Valencia <datacristian1523@gmail.com>
2024-01-17 15:50:10 -0500

    adding jwt token deendencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 const conectarDB = require("./config/db");
+const jwt = require("jsonwebtoken");
 
 conectarDB();
 
